7d12fa236019ca4413e51a8b5717ff84641120ad
xiger78 <xiger78@gmail.com>
2017-03-25 00:21:10 +0900

    Update common.php

--- common.php
+++ common.php
@@ -1,7 +1,7 @@
 <?
 $DB_SERVER = 'localhost';
-$USERID = 'root';
-$PASSWD = 'MediaCenter';
+$USERID = 'xxxxx';
+$PASSWD = 'xxxxxr';
 $DB='torrentdata';
 $cid= mysql_connect($DB_SERVER,$USERID,$PASSWD) or die(mysql_error()) ;
 mysql_select_db($DB,$cid) or die(mysql_error());
