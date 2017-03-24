612c1037fbafbd6650a1ae1998f99f2e12f20713
xiger78 <xiger78@gmail.com>
2017-03-25 00:21:23 +0900

    Update common.php

--- common.php
+++ common.php
@@ -1,7 +1,7 @@
 <?
 $DB_SERVER = 'localhost';
 $USERID = 'xxxxx';
-$PASSWD = 'xxxxxr';
+$PASSWD = 'xxxxxx';
 $DB='torrentdata';
 $cid= mysql_connect($DB_SERVER,$USERID,$PASSWD) or die(mysql_error()) ;
 mysql_select_db($DB,$cid) or die(mysql_error());
