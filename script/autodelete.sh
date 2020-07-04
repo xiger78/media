87f364c384c6ae3b79f7d0fcb9a2e507bf3e76fa
xiger78 <xiger78@gmail.com>
2020-07-04 09:45:47 +0900

    Update autodelete.sh

--- script/autodelete.sh
+++ script/autodelete.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
+
+############################
+# user id : pi
+# transmission passwod : raspberry
+#
+#example)
+# pi:raspberry
+############################
+
+#Change directory access authority
+sudo chmod 777 -R /mnt/sda1
+
+#Change directory owning group
+sudo chgrp -R pi /mnt/sda1/Downloads
+
+#Change directory owner
+sudo chown -R pi /mnt/sda1/Downloads
+
 SERVER="9091"
 
-TORRENTLIST=`transmission-remote $SERVER --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1`
+TORRENTLIST=`transmission-remote $SERVER -n pi:raspberry --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1`
 for TORRENTID in $TORRENTLIST
 do
-    DL_COMPLETED=`transmission-remote $SERVER --torrent $TORRENTID --info | grep "Percent Done: 100%"`
-    STATE_STOPPED=`transmission-remote $SERVER --torrent $TORRENTID --info | grep "State: Seeding\|Stopped\|Finished\|Idle"`
+    DL_COMPLETED=`transmission-remote $SERVER -n pi:raspberry --torrent $TORRENTID --info | grep "Percent Done: 100%"`
+    FILE_NAME=`transmission-remote $SERVER -n pi:raspberry --torrent $TORRENTID --info | grep "Name:"`
+    MAG_NET=`transmission-remote $SERVER -n pi:raspberry --torrent $TORRENTID --info | grep "Magnet:"`
+    FILE_SIZE=`transmission-remote $SERVER -n pi:raspberry --torrent $TORRENTID --info | grep "Have:"`
+    STATE_STOPPED=`transmission-remote $SERVER -n pi:raspberry --torrent $TORRENTID --info | grep "State: Seeding\|Stopped\|Finished\|Idle"`
     if [ "$DL_COMPLETED" ] && [ "$STATE_STOPPED" ]; then
-        transmission-remote $SERVER --torrent $TORRENTID --remove
+        python /home/pi/autosend.py "$FILE_NAME" "$MAG_NET" "$FILE_SIZE"
+        transmission-remote $SERVER -n pi:pi --torrent $TORRENTID --remove
     fi
 done
